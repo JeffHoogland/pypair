867eb40eda1ef681a7f6d71111083284b4837d88
Jeff Hoogland <jeffhoogland@linux.com>
2014-10-10 01:21:57 -0500

    Pairings now use networkx to get true swiss pairings

--- pypair.py
+++ pypair.py
@@ -2,11 +2,14 @@
 A tool for pairing players in a swiss event
 '''
 
+#Load our library for building/working with graphs
+import networkx as nx
 #Library for loading player dumps
 import cPickle as pickle
 import random
 
 dbg = True
+debuglevel = 1
 
 class Tornament(object):
     def __init__( self, startingTable=1 ):
@@ -24,10 +27,19 @@ class Tornament(object):
         '''
         
         self.playersDict = {}
-        self.currentRound = 1
+        self.currentRound = 0
         self.openTable = 0
         self.startingTable = startingTable
         self.roundPairings = {}
+        
+        #this defines the max number of players in a network point range before we split it up. Lower the number, faster the calculations
+        self.MaxGroup = 50 
+        
+        #Contains lists of players sorted by how many points they currently have
+        self.pointLists = {}
+        
+        #Contains a list of points in the event from high to low
+        self.pointTotals = []
         
     def addPlayer( self, DCINumber, playerName ):
         self.playersDict[DCINumber] = {  "Name": playerName,
@@ -53,24 +65,34 @@ class Tornament(object):
             
             4.) Check for left over players and assign a pair down
         """
+        self.currentRound += 1
         
         #Clear old round pairings
         self.roundPairings = {}
         self.openTable = self.startingTable
         
         #Contains lists of players sorted by how many points they currently have
-        pointLists = {}
+        self.pointLists = pointLists = {}
         
         #Contains a list of points in the event from high to low
-        pointTotals = []
+        self.pointTotals = pointTotals = []
+        
+        self.countPoints = {}
         
         #Add all players to pointLists
         for player in self.playersDict:
             info = self.playersDict[player]
-            if info['Points'] not in pointLists:
-                pointLists[info['Points']] = []
+            if "%s_1"%info['Points'] not in pointLists:
+                pointLists["%s_1"%info['Points']] = []
+                self.countPoints[info['Points']] = 1
             
-            pointLists[info['Points']].append(player)
+            #Breakers the players into groups of their current points up to the max group allowed.
+            #Smaller groups mean faster calculations
+            if len(pointLists["%s_%s"%(info['Points'], self.countPoints[info['Points']])]) > self.MaxGroup:
+                self.countPoints[info['Points']] += 1
+                pointLists["%s_%s"%(info['Points'], self.countPoints[info['Points']])] = []
+                
+            pointLists["%s_%s"%(info['Points'], self.countPoints[info['Points']])].append(player)
             
         #Add all points in use to pointTotals
         for points in pointLists:
@@ -79,35 +101,54 @@ class Tornament(object):
             #Randomize the players in the list so the first player isn't always the first paired
             random.shuffle(pointLists[points])
             
-        pointTotals.sort(reverse=True)
+        pointTotals.sort(reverse=True, key=lambda s: int(s.split('_')[0]))
+        
+        printdbg( "Pointals after sorting high to low are: %s"%pointTotals, 1 )
 
         for points in pointTotals:
             printdbg(  points ) 
-            #While we have 2 or more players with the same points, pair them
-            while len(pointLists[points]) > 1:
-                ourPlayer = pointLists[points].pop(0)
-                ourOpponent = pointLists[points].pop(0)
-                
-                self.pairPlayers(ourPlayer, ourOpponent)
+            bracketGraph = nx.Graph()
+            bracketGraph.add_nodes_from(pointLists[points])
+            
+            printdbg( pointLists[points], 5 )
+            printdbg( bracketGraph.nodes(), 5 )
+            
+            for player in bracketGraph.nodes():
+                for opponent in bracketGraph.nodes():
+                    if opponent not in self.playersDict[player]["Opponents"] and player != opponent:
+                        wgt = 1
+                        if self.playersDict[player]["Points"] > points or self.playersDict[opponent]["Points"] > points:
+                            wgt = 2
+                        bracketGraph.add_edge(player, opponent, weight=wgt)
+                        
+            pairings = nx.max_weight_matching(bracketGraph)
+            
+            printdbg( pairings, 3 )
+            
+            for p in pairings:
+                if p in pointLists[points]:
+                    self.pairPlayers(p, pairings[p])
+                    pointLists[points].remove(p)
+                    pointLists[points].remove(pairings[p])
                 
             #Check if we have an odd man out that we need to pair down
-            if len(pointLists[points]) == 1:
+            if len(pointLists[points]) > 0:
                 #Check to make sure we aren't at the last player in the event
-                printdbg(  "Player %s left in %s. The index is %s and the length of totals is %s"%(pointLists[points][0], points, pointTotals.index(points), len(pointTotals)))
+                printdbg(  "Player %s left in %s. The index is %s and the length of totals is %s"%(pointLists[points][0], points, pointTotals.index(points), len(pointTotals)), 1)
                 if pointTotals.index(points) + 1 == len(pointTotals):
-                    self.assignBye(pointLists[points].pop(0))
+                    while len(pointLists[points]) > 0:
+                        self.assignBye(pointLists[points].pop(0))
                 else:
+                    #Add our player to the next point group down
                     nextPoints = pointTotals[pointTotals.index(points) + 1]
                     
-                    ourPlayer = pointLists[points].pop(0)
-                    ourOpponent = pointLists[nextPoints].pop(0)
-                    
-                    self.pairPlayers(ourPlayer, ourOpponent)
+                    while len(pointLists[points]) > 0:
+                        pointLists[nextPoints].append(pointLists[points].pop(0))
                     
         return self.roundPairings
                 
     def pairPlayers( self, p1, p2 ):
-        printdbg("Pairing players %s and %s"%(p1, p2))
+        printdbg("Pairing players %s and %s"%(p1, p2), 5)
         
         self.playersDict[p1]["Opponents"].append(p2)
         self.playersDict[p2]["Opponents"].append(p1)
@@ -117,7 +158,7 @@ class Tornament(object):
         self.openTable += 1
 
     def assignBye( self, p1, reason="bye" ):
-        printdbg( "%s got the bye"%p1)
+        printdbg( "%s got the bye"%p1, 2)
         self.roundPairings[p1] = reason
         self.playersDict[p1]["Results"].append([0,0,0])
         
@@ -137,12 +178,19 @@ class Tornament(object):
         else:
             if result[0] > result[1]:
                 self.playersDict[p1]["Points"] += 3
+                printdbg("Adding result %s for player %s"%(result, p1), 2)
                 self.playersDict[p1]["Results"].append(result)
+                otresult = [result[1], result[0], result[2]]
+                printdbg("Adding result %s for player %s"%(otresult, p2), 2)
+                self.playersDict[p2]["Results"].append(otresult)
             elif result[1] > result[0]:
                 self.playersDict[p2]["Points"] += 3
-                result[0], result[1] = result[1], result[0]
-                self.playersDict[p2]["Results"].append(result)
+                printdbg("Adding result %s for player %s"%(result, p1), 2)
+                self.playersDict[p1]["Results"].append(result)
+                otresult = [result[1], result[0], result[2]]
+                printdbg("Adding result %s for player %s"%(otresult, p2), 2)
+                self.playersDict[p2]["Results"].append(otresult)
 
-def printdbg( msg ):
-    if dbg == True:
+def printdbg( msg, level=1 ):
+    if dbg == True and level <= debuglevel:
         print msg
--- test.py
+++ test.py
@@ -2,11 +2,14 @@
 
 from pypair import Tornament
 import random
+import os
+
+home = os.path.expanduser("~")
 
 to = Tornament()
 
-for p in range(17):
-    to.addPlayer( p, "Timmy" )
+for p in range(4501):
+    to.addPlayer( str(p), "Timmy" )
 
 pairings1 = to.pairRound()
 
@@ -26,10 +29,11 @@ for table in pairings1:
         else:
             to.reportMatch(table, [1,1,1])
 
-to.saveEventData("/home/jeff/datadump1.txt")
+to.saveEventData("%s/datadump1.txt"%home)
 
-#for p in to.playersDict:
-#    print "%s has %s points"%(p, to.playersDict[p]["Points"])
+print ""
+print to.playersDict['1']
+print ""
 
 pairings2 = to.pairRound()
 
@@ -49,13 +53,11 @@ for table in pairings2:
         else:
             to.reportMatch(table, [1,1,1])
 
-to.saveEventData("/home/jeff/datadump2.txt")
+to.saveEventData("%s/datadump2.txt"%home)
 
-#print ""
-#print ""
-#print ""
-#for p in to.playersDict:
-#    print "%s has %s points"%(p, to.playersDict[p]["Points"])
+print ""
+print to.playersDict['1']
+print ""
 
 pairings3 = to.pairRound()
 
@@ -75,4 +77,56 @@ for table in pairings3:
         else:
             to.reportMatch(table, [1,1,1])
 
-to.saveEventData("/home/jeff/datadump3.txt")
+to.saveEventData("%s/datadump3.txt"%home)
+
+print ""
+print to.playersDict['1']
+print ""
+
+pairings4 = to.pairRound()
+
+for table in pairings4:
+    if not type(pairings4[table]) is str:
+        per = random.randint(1, 100)
+        if per < 25:
+            to.reportMatch(table, [2,0,0])
+        elif per < 47:
+            to.reportMatch(table, [2,1,0])
+        elif per < 60:
+            to.reportMatch(table, [0,2,0])
+        elif per < 97:
+            to.reportMatch(table, [1,2,0])
+        elif per < 98:
+            to.reportMatch(table, [0,0,1])
+        else:
+            to.reportMatch(table, [1,1,1])
+
+to.saveEventData("%s/datadump4.txt"%home)
+
+print ""
+print to.playersDict['1']
+print ""
+
+pairings5 = to.pairRound()
+
+for table in pairings5:
+    if not type(pairings5[table]) is str:
+        per = random.randint(1, 100)
+        if per < 25:
+            to.reportMatch(table, [2,0,0])
+        elif per < 47:
+            to.reportMatch(table, [2,1,0])
+        elif per < 60:
+            to.reportMatch(table, [0,2,0])
+        elif per < 97:
+            to.reportMatch(table, [1,2,0])
+        elif per < 98:
+            to.reportMatch(table, [0,0,1])
+        else:
+            to.reportMatch(table, [1,1,1])
+
+to.saveEventData("%s/datadump5.txt"%home)
+
+print ""
+print to.playersDict['1']
+print ""
